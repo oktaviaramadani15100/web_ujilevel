3a1bb6f009c9724fe82f13d65b33633372d6412e
oktaviaramadani <ramadanioktavia@gmail.com>
2022-09-19 19:35:11 +0700

    tambah php

---
+++ penyimpanan.php
@@ -0,0 +1,21 @@
+<?php
+include 'koneksi.php';
+
+if(isset($_POST['simpan'])){
+    $Nama_siswa = $_POST ['Nama_siswa'];
+    $kelas = $_POST ['kelas'];
+    $kode_barang = $_POST ['kode_barang'];
+    $Nama_barang = $_POST ['Nama_barang'];
+    $stock_barang = $_POST ['stock_barang'];
+
+    $sql = "INSERT INTO data_barang(Nama_siswa, kelas, kode_barang, Nama_barang, stock_barang) VALUE ('$Nama_siswa, $kelas, $kode_barang, $Nama_barang, $stock_barang)";
+
+    $query = mysqli_query($connect, $query);
+
+    if($query){
+        header('Location: tampilandatasiswa.php');
+    }else{
+        header('Location: penyimpanan.php?status=gagal');
+    }
+}
+?>
